4ad01acdaa6adf96d5640ca73e42d8a88c7ec345
colenocks <enockscoleman@gmail.com>
2020-06-14 04:14:00 +0100

    fix: H10-App Crashed Error, dotenv-heroku clash

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const { spawn, exec } = require("child_process");
 const express = require("express");
